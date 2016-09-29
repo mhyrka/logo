7125c14c9e20adc299360d6c11f2f69b5602dd09
CJ R <hello@cjr.co.de>
2016-09-28 22:15:56 -0600

    :arrows_counterclockwise::balloon: refactor 22 solution

--- 22_jsonFormatting/solutions/CJ.js
+++ 22_jsonFormatting/solutions/CJ.js
@@ -1,37 +1,45 @@
-var fs = require('fs');
+const fs = require('fs');
 
 fs.readFile('./../books.json', 'utf8', function (error, json) {
-  var books = JSON.parse(json).data;
-  var bookProperties = ['book_id', 'title', 'genre', 'description', 'cover'];
-  var authorProperties = ['author_id', 'first_name', 'last_name', 'biography', 'portrait'];
-  var bookLookup =  {};
-  var authorLookup = {};
+  const books = JSON.parse(json).data;
 
-  var result = books.reduce(function(result, row){
-    var book = bookLookup[row.title];
-    if(!book){
-      book = bookProperties.reduce(function(book, property){
-        book[property] = row[property]
-        return book;
-      }, {});
-      book.authors = [];
-      bookLookup[book.title] = book;
-      result.push(book);
+  const bookProperties = ['book_id', 'title', 'genre', 'description', 'cover'];
+  const authorProperties = ['author_id', 'first_name', 'last_name', 'biography', 'portrait'];
+  const cache = {};
+
+  function setProperty(data){
+    return function(object, property) {
+      object[property] = data[property]
+      return object;
+    }
+  }
+
+  function getBook(row) {
+    return cache[row.title] || bookProperties.reduce(setProperty(row), { authors: [] });
+  }
+
+  function getAuthor(row) {
+    return cache[row.first_name + row.last_name] || (() => {
+      const author = authorProperties.reduce(setProperty(row), {});
+      cache[author.first_name + author.last_name] = author;
+      return author;
+    })();
+  }
+
+  const results = books.reduce(function(results, row){
+    const book = getBook(row);
+
+    if(!cache[row.title]) {
+      cache[row.title] = book;
+      results.push(book);
     }
 
-    var author = authorLookup[row.first_name + row.last_name];
-    if(!author){
-      var author = authorProperties.reduce(function(author, property){
-        author[property] = row[property]
-        return author;
-      }, {});
-      authorLookup[row.first_name + row.last_name] = author;
-    }
+    const author = getAuthor(row);
 
     book.authors.push(author);
 
-    return result;
+    return results;
   }, []);
 
-  console.log(JSON.stringify(result));
-})
+  fs.writeFile('books.json', JSON.stringify(results));
+});
