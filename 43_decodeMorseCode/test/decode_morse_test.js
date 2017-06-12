56d0771dc7126308b815e19d0b8758d8753d4a5a
coloradokim <schlesinger.kim@gmail.com>
2017-06-12 15:15:17 -0600

    add package.json

--- 43_decodeMorseCode/test/decode_morse_test.js
+++ 43_decodeMorseCode/test/decode_morse_test.js
@@ -1,6 +1,6 @@
 let chai = require('chai');
 let assert = chai.assert;
-let decodeMorse = require('../src/decode_morse');
+let decodeMorse = require('../decode_morse');
 
 describe('Decode Morse Code', () => {
   it('translates a string of morse code', () => {
