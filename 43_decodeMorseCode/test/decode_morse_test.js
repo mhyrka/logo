f12b75d1c410e59b42847ff5559607d71cb2be6b
coloradokim <schlesinger.kim@gmail.com>
2017-06-12 15:03:59 -0600

    update file names

--- 43_decodeMorseCode/test/decode_morse_test.js
+++ 43_decodeMorseCode/test/decode_morse_test.js
@@ -1,6 +1,6 @@
 let chai = require('chai');
 let assert = chai.assert;
-let decodeMorse = require('../src/decode_morse_code');
+let decodeMorse = require('../src/decode_morse');
 
 describe('Decode Morse Code', () => {
   it('translates a string of morse code', () => {
