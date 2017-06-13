8c4f82e7dd4d22f7b821fc4d1131a3598308d72a
Kim Schlesinger <coloradokim@users.noreply.github.com>
2017-06-12 19:39:12 -0600

    fix test input

--- 43_decodeMorseCode/test/decode_morse_test.js
+++ 43_decodeMorseCode/test/decode_morse_test.js
@@ -4,7 +4,7 @@ let decodeMorse = require('../decode_morse');
 
 describe('Decode Morse Code', () => {
   it('translates a string of morse code', () => {
-    assert.equal(decodeMorse('.... . .-.. .-.. --- / .-- --- .-. .-.. -..'), 'HEY JUDE');
+    assert.equal(decodeMorse('.... . -.-- / .--- ..- -.. .'), 'HEY JUDE');
     assert.equal(decodeMorse('.... . .-.. .-.. --- / .-- --- .-. .-.. -..'), 'HELLO WORLD');
     assert.equal(decodeMorse('.. -. -.. .. ...- .. -.. ..- .- .-.. ... / .- -. -.. / .. -. - . .-. .- -.-. - .. --- -. ... / --- ...- . .-. / .--. .-. --- -.-. . ... ... . ... / .- -. -.. / - --- --- .-.. ...'),'INDIVIDUALS AND INTERACTIONS OVER PROCESSES AND TOOLS');
   })
