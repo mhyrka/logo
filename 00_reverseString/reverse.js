3c1c552793f3864a442aed9633ef7570e18a684b
Matthew Hyrka <mhyrka@gmail.com>
2018-02-08 10:37:08 -0700

    1st solution DP challenge

--- 00_reverseString/reverse.js
+++ 00_reverseString/reverse.js
@@ -1,5 +1,11 @@
 function reverse(word) {
-  // YOUR CODE HERE
+  var splitWord = word.split("");
+  var reverseArray = splitWord.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
 }
+console.log(reverse('hello world'));
+
+
 
 module.exports = reverse
